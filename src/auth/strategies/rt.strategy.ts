ee09ef40fee77819d4d7f3a683744eda38e91af1
Oleksii Shevchuk <oleksapershyi@gmail.com>
2023-01-14 16:04:58 +0100

    Add auth strategies

---
+++ src/auth/strategies/rt.strategy.ts
@@ -0,0 +1,28 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get<string>('RT_SECRET'),
+      passReqToCallback: true,
+    });
+  }
+
+  validate(request: Request, payload: any) {
+    const refreshToken = request
+      .get('authorization')
+      .replace('Bearer', '')
+      .trim();
+
+    return {
+      ...payload,
+      refreshToken,
+    };
+  }
+}
